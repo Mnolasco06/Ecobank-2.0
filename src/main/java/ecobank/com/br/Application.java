8fbf90522d51ae44bf337a0f8fee7f328306178a
Mnolasco06 <mnolascomojica@gmail.com>
2024-07-25 10:51:01 -0400

    pom modification - scam components

--- src/main/java/ecobank/com/br/Application.java
+++ src/main/java/ecobank/com/br/Application.java
@@ -4,8 +4,10 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL")})
+@ComponentScan
 @SpringBootApplication
 public class Application {
 
